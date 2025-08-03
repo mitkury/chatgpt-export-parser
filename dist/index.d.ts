3f740a138dfd164f3c811957dc8cce12b19ddcdd
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-08-03 16:09:29 +0300

    fix: remove dist/ from git tracking and add to .gitignore
    - Add dist/ to .gitignore to prevent build output from being committed
    - Remove dist/ directory from git tracking
    - Build output should be generated locally, not committed to repository

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { parseExport } from './parser';
-export type { ExportData, ParsedConversation, ParsedMessage, ConversationExport, MessageNode, MessageContent, MessageTree, ConversationBranch } from './types';
-export { ChatGPTExportSchema, ConversationSchema, MessageSchema, NodeSchema, AuthorSchema, ContentSchema, validateConversations, validateUser, validateMessageFeedback, validateModelComparisons, validateSharedConversations } from './schema';
-//# sourceMappingURL=index.d.ts.map
--- dist/parser.d.ts
+++
@@ -1,3 +0,0 @@
-import { ExportData } from './types';
-export declare function parseExport(zipPath: string): Promise<ExportData>;
-//# sourceMappingURL=parser.d.ts.map
